882471ff63e3bedcfe94fcc619a1352ddde1c80c
Tom McLaughlin <tmclaughlin@dmsi.com>
2022-08-05 09:49:54 -0500

    allow GITHUB_PAT variable

--- update_container_hashes.sh
+++ update_container_hashes.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-
-if ! command -v bw &> /dev/null ; then
-	<&2 echo "Expecting bitwarden cli (bw) to be available with a github pat stored under \"github pat\""	
-	exit 1
+if [ -z "$GITHUB_PAT" ]; then
+	# try bitwarden cli
+	if ! command -v bw &> /dev/null ; then
+		>&2 echo "Expecting bitwarden cli (bw) to be available with a github pat stored under \"github pat\""	
+		exit 1
+	fi
 fi
 
-github_pat=$(bw get password "github pat")
+GITHUB_PAT=$(bw get password "github pat")
 owner=dmsi-io
 branch=develop
 
@@ -20,10 +22,9 @@ repos[houston-api-docs]=main
 for entry in "${!repos[@]}"; do
 	repo=$entry
 	branch="${repos[$entry]}"
-	hash=$(curl -s -H "Authorization: token $github_pat" \
+	hash=$(curl -s -H "Authorization: token $GITHUB_PAT" \
 		-H "Accept: application/vnd.github.v3.sha" \
 		"https://api.github.com/repos/$owner/$repo/commits/$branch")
-	gsed -i -e "s/$repo:[a-f0-9]\{7\}/$repo:${hash:0:7}/" docker-compose.yaml
-done
-
+			gsed -i -e "s/$repo:[a-f0-9]\{7\}/$repo:${hash:0:7}/" docker-compose.yaml
+		done
 
